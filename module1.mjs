22f5ebda0c766baff498fd6918fabd81252c2440
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-02-01 08:02:21 -0500

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import wzy, {a, c, d}  from './module2.mjs'
+console.log(wzy);
+console.log(c);
+console.log(d);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Dimple";
+const b = "Sharv";
+const c = "Sweety";
+const d = "Mansi";
+
+export default b;  
+export {a};  
+export {c};  
+export {d};  
