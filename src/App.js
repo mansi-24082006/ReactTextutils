d8e371dea0e0994361db8ec88d26a1787c9502a6
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-02-03 07:11:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import { BrowserRouter as Router,
-//           Routes,
-//           Route,
-//         } from "react-router-dom";
-
+import { BrowserRouter as Router,
+          Routes,
+          Route,
+        } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
@@ -22,35 +21,46 @@ function App() {
           setAlert(null);
       }, 3000);
   }
+  const removeBodyClasses= (() => {
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-success')
 
-  const toggleMode = ()=>{
+  })
+  const toggleMode = (cls)=>{
+    removeBodyClasses();
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      document.body.style.color = 'white';
       showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+
       showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()}/>
-    <Alert alert={alert}/>
+    <Alert alert={alert} />
     <div className="container my-3">
-      {/* <Routes>
-        <Route path = "/about"> */}
-          {/* <About/> */}
-        {/* </Route>
-        <Route path = "/"> */}
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below"  mode={mode}/>
-        {/* </Route>
-      </Routes> */}
+      <Routes>
+        <Route path = "/about" element={<About />} /> 
+         
+        <Route path = "/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces"  mode={mode}/>
+       } />
+      </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
+    {/* <About mode={mode}/> */}
     </>
     );
 }
